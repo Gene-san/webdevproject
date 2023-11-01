47f629bf855ee66f23752cba0ef7c626ab3bed9a
Gene-san <genealprincebraga@gmail.com>
2023-11-01 09:48:48 +0800

    Update delete.php

--- functions/delete.php
+++ functions/delete.php
@@ -1,7 +1,7 @@
 <?php
 include_once("../dbConnection/mysqlconfig_connection.php");
 $id = $_GET['id'];
-mysqli_query($dbc, "DELETE FROM tblsubject WHERE subject_id='$id'");
+mysqli_query($dbc, "DELETE FROM tblsubjects WHERE Subject_ID='$id'");
 header("Location: ../index.php");
 
 ?>
