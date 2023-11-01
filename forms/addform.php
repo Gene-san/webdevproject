a979b298b4b5f33f260f2342b5dc89e88693bb5c
Gene-san <genealprincebraga@gmail.com>
2023-11-01 09:16:20 +0800

    Update addform.php

--- forms/addform.php
+++ forms/addform.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Date</title>
+    <title>Add Data</title>
 </head>
 <body>
     <h1>Add Subject</h1>
